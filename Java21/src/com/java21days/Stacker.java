e5bb4d0c80d52cc164f67fef7833e257dbfee5e2
QuantumArchive <chrbruner9@gmail.com>
2018-04-23 21:47:21 -0700

    written exercise 2

---
+++ Java21/src/com/java21days/Stacker.java
@@ -0,0 +1,49 @@
+package com.java21days;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Stacker extends JFrame {
+    public Stacker() {
+        super("Stacker");
+        setSize(430, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        JPanel commandPane = new JPanel();
+        BoxLayout horizontal = new BoxLayout(commandPane,
+            BoxLayout.X_AXIS);
+        commandPane.setLayout(horizontal);
+        JButton subscribe = new JButton("Subscribe");
+        JButton unsubscribe = new JButton("Unsubscribe");
+        JButton refresh = new JButton("Refresh");
+        JButton save = new JButton("Save");
+        commandPane.add(subscribe);
+        commandPane.add(unsubscribe);
+        commandPane.add(refresh);
+        commandPane.add(save);
+        JPanel textPane = new JPanel();
+        JTextArea text = new JTextArea(4, 70);
+        JScrollPane scrollPane = new JScrollPane(text);
+        FlowLayout flow = new FlowLayout();
+        setLayout(flow);
+        add(commandPane);
+        add(scrollPane);
+        setVisible(true);
+    }
+    
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception exc) {
+            System.err.println("Couldn't use the system "
+                + "look and feel: " + exc);
+        }
+    }
+    
+    public static void main(String[] arguments) {
+        Stacker at = new Stacker();
+    }
+}
